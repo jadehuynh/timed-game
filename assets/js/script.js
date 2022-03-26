67c77fa0a314b3b35a70168212f3516ca1f6ff21
jadehuynh <jadehuynh08@gmail.com>
2022-03-26 15:35:12 -0500

    fixed timer display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,32 @@
+var rootEl = $('#root');
+
 var welcomeTxt= document.querySelector('.lrgTitle')
 var rules= document.querySelector('.intro-Rules')
 var beginGame= document.querySelector('.start-btn')
 var timeLeft= 5
 var blank= document.querySelector('#blankSpace')
-var timeDisplay= document.querySelector('timerStart')
-var quest= document.querySelector('questions')
+var timeDisplay= document.querySelector('.timerStart')
+var quest= document.querySelector('.questions')
 
 
 
 
 
 
-beginGame.addEventListener('click', function() {
+beginGame.addEventListener('click', function () {
+
     var timer = setInterval(function () {
-        timeLeft--; 
-            if(timeLeft < 0){
+        timeLeft--;
+        if (timeLeft < 0) {
             clearInterval(timer)
-         }else if (timeLeft === 0){
+        }
+        else if (timeLeft === 0) {
             alert('Game Over!')
             timeDisplay.textContent= timeLeft
             timeLeft.textContent = ""
-         }else {
-            timeDisplay.textContent = blank
+        }
+        else (
+            timeDisplay.textContent = timeLeft
         
-    )},1000)
-}
+     )} , 1000)
+})
