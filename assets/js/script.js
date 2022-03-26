e5e62ac04ba9f950a00e549912ce0b498172dc97
jadehuynh <jadehuynh08@gmail.com>
2022-03-26 17:33:25 -0500

    added reset folder and some CSS styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ var quest= document.querySelector('.questions')
 
 
 
-
 const procedures = document.createElement("p");
-rules.appendChild(procedures).textContent= alert('This is a multiple choice game! Please read the question carefuly before selecting ONE of the FOUR answers provided to proceed. If you finish answering all the given questions before the timer reaches ZERO, you win! Although, if you DO NOT finish within the alotted time- YOU LOSE!')
+rules.appendChild(procedures).textContent= alert('Welcome to the TIMED QUIZ GAME! This is a multiple choice game. Please read the question(s) carefully before selecting ONE of the FOUR answers provided to proceed. If you finish answering all the given questions before the timer reaches ZERO, you win! Although, if you DO NOT finish within the alotted time- YOU LOSE!')
 
+const secondsLeft = document.createElement("h3");
+timeDisplay.appendChild(secondsLeft).textContent= ('seconds remaining!!!')
 
 
 
@@ -35,6 +36,9 @@ beginGame.addEventListener('click', function () {
          
 })
 
+
+const questTitle= document.createElement("h3");
+quest.appendChild(questTitle).textContent= ('QUESTIONS')
 
 const hoiNgu = document.createElement("p");
 quest.appendChild(hoiNgu).textContent= ('Test.')
