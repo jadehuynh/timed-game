a7c643bda3f1f3f63c0784b725b5c0d1a459a7f6
jadehuynh <jadehuynh08@gmail.com>
2022-03-27 13:30:11 -0500

    added css styling attributes and upated div sections

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -1,2 +1,2 @@
-var quizQuests = ["What is the correct "];
-var quizAns = [];
+var quizQuests = ["What is the correct abbreviation for JavaScript? ", "What does 'var' stand for in JavaScript Language?", "What kind of special characters follow the word 'function'?", "What kind of data type involves true or false conditions?", "What is the purpose of the 'concat()' method?"];
+var quizAns = ["JS", "'Variable", "parenthesis", "Boolean", "To join"];
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var rules= document.querySelector('.intro-Rules')
 var beginGame= document.querySelector('.start-btn')
 var timeLeft= 5
 var timeDisplay= document.querySelector('.timerStart')
-var quest= document.querySelector('.questions')
+var quest= document.querySelector('#questions')
 
 
 //made prompt with rules for creating an element to hold the text
@@ -35,12 +35,11 @@ beginGame.addEventListener('click', function () {
          
 })
 
+
+
 //created h3 element to display time remaining
 const secondsLeft = document.createElement("h3");
 timeDisplay.appendChild(secondsLeft).textContent= ('time remaining!!!')
 //created h3 element to display questions
 const questTitle= document.createElement("h3");
 quest.appendChild(questTitle).textContent= ('QUESTIONS')
-//created h3 element to display text content
-const hoiNgu = document.createElement("p");
-quest.appendChild(hoiNgu).textContent= ('Test.')
