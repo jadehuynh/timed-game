ab497457847b75983c3a513541112a0316a76fa5
jadehuynh <jadehuynh08@gmail.com>
2022-03-27 16:33:25 -0500

    creating functions if/else

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,80 @@ var rootEl = $('#root');
 var welcomeTxt= document.querySelector('.lrgTitle')
 var rules= document.querySelector('.intro-Rules')
 var beginGame= document.querySelector('.start-btn')
-var timeLeft= 5
+var timeLeft= 30
 var timeDisplay= document.querySelector('.timerStart')
 var quest= document.querySelector('#questions')
 var container= document.querySelector('#content')
 var userChoice= document.querySelector('.answers')
+var questionContainer= document.querySelector('#questionContainer')
+var nextBtn= document.querySelector('nextBtn')
+questionContainer.style.display= "none"
 
-var quizQuests = [
-    "What is the correct abbreviation for JavaScript? ", 
-    "What does 'var' stand for in JavaScript Language?", 
-    "What kind of special characters follow the word 'function'?", 
-    "What kind of data type involves true or false conditions?", 
-    "What is the purpose of the 'concat()' method?"];
+var questions= [
+    {
+        question: "7 x 7",
+        answers: ["49", "59", "69", "79"],
+        cAnswer: "49"
+    },
+    {
+        question: "8 x 8",
+        answers: ["49", "59", "69", "64"],
+        cAnswer: "64"
+    },
+    {
+        question: "9 x 9",
+        answers: ["49", "59", "81", "79"],
+        cAnswer: "81"
+    },
+]
+var questionIndex= 0
 
-var quizAns = [
-    "JS", 
-    "'Variable", 
-    "Parenthesis & Curly Brackets", 
-    "Boolean", 
-    "To join"];
+function showQuestion(index) {
+    for (var i = 0; i < questions.length; i++) {
+        var Q= (questions[index].question)
+        var answerList= (questions[index].answers)
+        quest.textContent= Q
+    }
+    answerList.forEach(function(ans) {
+        var answerButton= document.createElement("button")
+            answerButton.classList.add("answer-btn")
+            answerButton.textContent= ans
+            userChoice.appendChild(answerButton)
+            answerButton.addEventListener('click', function () {
+                //console.log(answerButton.textContent)
+                ansCheck(answerButton.textContent)
+            })
+            
+    })
+}
+    
+var checker= document.createElement("h2")
+function ansCheck(answer) {
+    
+        if (answer === questions[questionIndex].cAnswer) {
+            console.log("correct")
+            checker.textContent= "Correct!"
+        }
+            else {
+                console.log("wrong")
+                checker.textContent= "Incorrect."
+            }
+            questionContainer.appendChild(checker)
+            questionIndex++;
+            next(questionIndex)
+}
+
+function next(index) {
+    nextBtn.addEventListener('click', function() {
+       if (index >= questions.length) {
+           console.log()
+       }
+       else {
+        checker.textContent= " "
+        showQuestion(index)
+       }
+    })
+}
 
 
 
@@ -34,7 +89,8 @@ rules.appendChild(procedures).textContent= alert('Welcome to the TIMED QUIZ GAME
 
 //start button function to also begin timer
 beginGame.addEventListener('click', function () {
-
+    questionContainer.style.display= "block"
+    showQuestion(questionIndex)
     var timer = setInterval(function () { 
         timeLeft--;
         if (timeLeft < 0) {
@@ -53,27 +109,27 @@ beginGame.addEventListener('click', function () {
        
 })
 
-function showQuestion() {
-//I need to figure out how to interchange the array of questions in and out
-    var questionIndex = Math.floor(Math.random() * quizQuests.length);
+// function showQuestion() {
+// //I need to figure out how to interchange the array of questions in and out
+//     var questionIndex = Math.floor(Math.random() * quizQuests.length);
 
-beginGame.addEventListener('click', questionIndex)
-quest.appendChild(quest).textContent= (quizQuests[i])
+// beginGame.addEventListener('click', questionIndex)
+// quest.appendChild(quest).textContent= (quizQuests[i])
 
 
 
-//I need to figure out how to all boolean conditions to all answer selections and how to interchange them with the array
-if (userChoice === true) {
-    window.alert("Correct answer!");
+// //I need to figure out how to all boolean conditions to all answer selections and how to interchange them with the array
+// if (userChoice === true) {
+//     window.alert("Correct answer!");
   
-} else if (
-    (userChoice === false)
+// } else if (
+//     (userChoice === false)
 
-  ) {
-    window.alert("Incorrect. Please try again.");
-  }
-console.log(showQuestion)
-}
+//   ) {
+//     window.alert("Incorrect. Please try again.");
+//   }
+// console.log(showQuestion)
+// }
 
 //created h3 element to display questions
 const questTitle= document.createElement("h3");
