15685362d93555cc0f4cea43ea50a23997049dfb
jadehuynh <jadehuynh08@gmail.com>
2022-03-27 15:13:06 -0500

    creating functions for interchanging questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,24 @@ var beginGame= document.querySelector('.start-btn')
 var timeLeft= 5
 var timeDisplay= document.querySelector('.timerStart')
 var quest= document.querySelector('#questions')
+var container= document.querySelector('#content')
+var userChoice= document.querySelector('.answers')
+
+var quizQuests = [
+    "What is the correct abbreviation for JavaScript? ", 
+    "What does 'var' stand for in JavaScript Language?", 
+    "What kind of special characters follow the word 'function'?", 
+    "What kind of data type involves true or false conditions?", 
+    "What is the purpose of the 'concat()' method?"];
+
+var quizAns = [
+    "JS", 
+    "'Variable", 
+    "Parenthesis & Curly Brackets", 
+    "Boolean", 
+    "To join"];
+
+
 
 
 //made prompt with rules for creating an element to hold the text
@@ -17,7 +35,7 @@ rules.appendChild(procedures).textContent= alert('Welcome to the TIMED QUIZ GAME
 //start button function to also begin timer
 beginGame.addEventListener('click', function () {
 
-    var timer = setInterval(function () {
+    var timer = setInterval(function () { 
         timeLeft--;
         if (timeLeft < 0) {
             clearInterval(timer)
@@ -32,14 +50,31 @@ beginGame.addEventListener('click', function () {
             timeDisplay.textContent = timeLeft
         
      )} , 1000)
-         
+       
 })
 
+function showQuestion() {
+//I need to figure out how to interchange the array of questions in and out
+    var questionIndex = Math.floor(Math.random() * quizQuests.length);
+
+beginGame.addEventListener('click', questionIndex)
+quest.appendChild(quest).textContent= (quizQuests[i])
 
 
-//created h3 element to display time remaining
-const secondsLeft = document.createElement("h3");
-timeDisplay.appendChild(secondsLeft).textContent= ('time remaining!!!')
+
+//I need to figure out how to all boolean conditions to all answer selections and how to interchange them with the array
+if (userChoice === true) {
+    window.alert("Correct answer!");
+  
+} else if (
+    (userChoice === false)
+
+  ) {
+    window.alert("Incorrect. Please try again.");
+  }
+console.log(showQuestion)
+}
+
 //created h3 element to display questions
 const questTitle= document.createElement("h3");
 quest.appendChild(questTitle).textContent= ('QUESTIONS')
